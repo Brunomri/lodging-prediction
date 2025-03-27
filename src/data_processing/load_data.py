7e5b517cd5902a4a9b95be3d477126f2a32f62ef
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-03-27 09:56:03 -0300

    Add one-hot encoding and label encoding.

--- src/data_processing/load_data.py
+++ src/data_processing/load_data.py
@@ -3,9 +3,11 @@ import glob
 from sklearn.preprocessing import LabelEncoder
 
 def create_dataframe():
+    # Build file paths for all csv files
     file_paths = glob.glob('data/*.csv')
 
     dfs = []
+    # Combine all csv files in a single data frame
     for file_path in file_paths:
         df = pd.read_csv(file_path)
 
@@ -13,24 +15,25 @@ def create_dataframe():
         city_name = file_name.split('_')[0]
         day_type = file_name.split('_')[1]
 
+        # Add city and day_type (weekday or weekend) as new columns
         df['city'] = city_name
         df['day_type'] = day_type
 
         dfs.append(df)
 
+    # Concatenate data frames from all csv files
     df = pd.concat(dfs, ignore_index=True)
     return df
 
 def clean_dataframe(df):
+    # Drop all rows with missing values
     return df.dropna()
 
 def encode_variables(df):
-    label_enc = LabelEncoder()
-    df['room_shared'] = label_enc.fit_transform(df['room_shared'])
-    df['room_private'] = label_enc.fit_transform(df['room_private'])
-    df['host_is_superhost'] = label_enc.fit_transform(df['host_is_superhost'])
-
-    df = pd.get_dummies(df, columns=["room_type"], drop_first=True)
+    # Use one-hot encoding for room_type, because it has 3 possible categories
+    df = pd.get_dummies(df, columns=["room_type"], drop_first=False)
+    binary_cols = ["room_shared", "room_private", "host_is_superhost"]
+    df[binary_cols] = df[binary_cols].astype(int)
     return df
 
 def process_data():

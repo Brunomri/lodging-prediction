25e9fad7a8ce99af9566c278763260cf4721816f
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-04-09 18:01:40 -0300

    Add scatter plot.

--- src/charts/plot_data.py
+++ src/charts/plot_data.py
@@ -5,3 +5,8 @@ def plot_heatmap(data):
     plt.figure(figsize = (24, 12))
     sns.heatmap(data = data, cmap = 'Blues', annot = True)
     plt.show()
+
+def plot_scatter(pred, test):
+    plt.figure(figsize = (24, 12))
+    sns.scatterplot(x = pred, y = test)
+    plt.show()

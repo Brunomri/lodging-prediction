18542f335614b0b708cb30558b39db063cda3ba4
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-04-09 18:02:04 -0300

    Call predictions from main.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import src.data_processing.load_data as load_data
+from src.data_processing.load_data import process_data
+from src.prediction.run_prediction import split_train_test, run_models
+
 
 def main():
-    df = load_data.process_data()
+    x, y = process_data()
+    x_train, x_test, y_train, y_test = split_train_test(x, y)
+    run_models(x_train, x_test, y_train, y_test)
 
 if __name__ == "__main__":
     main()

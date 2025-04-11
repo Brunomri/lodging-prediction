e0890c5a82614823d7c29e0cc11137d2746a385b
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-04-11 16:14:26 -0300

    Show data distribution in histogram and box plot.

--- src/charts/plot_data.py
+++ src/charts/plot_data.py
@@ -10,3 +10,13 @@ def plot_scatter(pred, test):
     plt.figure(figsize = (24, 12))
     sns.scatterplot(x = pred, y = test)
     plt.show()
+
+def plot_distribution(df):
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    df['realSum'].hist(bins=50)
+
+    plt.subplot(1, 2, 2)
+    df.boxplot(column='realSum')
+    plt.show()

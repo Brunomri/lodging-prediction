a85beb5288e401e10760872ddbc56cfddeb5eade
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-04-14 17:34:09 -0300

    Use log transform in target values, bring get_model_stats specifically to linear regression.

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -1,6 +1,15 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
+
+import statsmodels.api as sm
 
 def lr_predict(x_train, y_train, x_test):
     lr = LinearRegression()
-    lr.fit(x_train, y_train)
-    return lr.predict(x_test)
+    lr.fit(x_train, np.log1p(y_train))
+    get_model_stats(x_train, y_train)
+    return np.expm1(lr.predict(x_test))
+
+def get_model_stats(x_train, y_train):
+    X2 = sm.add_constant(x_train)
+    model_stats = sm.OLS(y_train.values.reshape(-1, 1), X2).fit()
+    print(model_stats.summary())

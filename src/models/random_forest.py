7f12f64196e6a30d01290240930417e31b268e94
Brunomri <19270033+Brunomri@users.noreply.github.com>
2025-04-17 11:55:30 -0300

    Tweak random forest arguments to reduce overfitting.

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 
 def rf_predict(x_train, y_train, x_test):
-    rf = RandomForestRegressor()
+    rf = RandomForestRegressor(n_estimators=200, max_depth=12, min_samples_leaf=5, min_samples_split=10)
     rf.fit(x_train, np.log1p(y_train))
     get_model_stats(rf)
     return np.expm1(rf.predict(x_train)), np.expm1(rf.predict(x_test))
